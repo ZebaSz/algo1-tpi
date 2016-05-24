93dc5147ef57a704cd4bab6e10ae57c5cd002991
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2016-05-23 21:56:38 -0300

    agregar test basico para tipo campo a main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include "campo.h"
 using namespace std;
 
 int main()
 {
+    Posicion posG;
+    posG.x = 25;
+    posG.y = 10;
+    Posicion posC;
+    posC.x = 14;
+    posC.y = 22;
+
+    Campo campo(posG, posC);
+    cout << campo << endl;
+
+    Campo otroCampo(posG, posC);
+
+    if(campo == otroCampo) {
+        cout << "Son el mismo" << endl;
+    } else {
+        cout << "No son el mismo" << endl;
+    }
+
+    ofstream out("test.txt");
+    if(out.is_open()) {
+        campo.guardar(out);
+        out.close();
+    }
+    
+
     return 0;
 }
