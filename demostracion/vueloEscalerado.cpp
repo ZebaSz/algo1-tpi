8210a6c24786a930852e2b4b972bc5e48bcfb4a7
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2016-06-09 20:16:45 -0300

    separar en 2 archivos, mejores secciones

---
+++ demostracion/vueloEscalerado.cpp
@@ -0,0 +1,32 @@
+bool Drone::vueloEscalerado() const
+{
+	bool escalerado = _enVuelo;
+	if(_trayectoria.size() > 2) {
+	int dirx = _trayectoria[0].x - _trayectoria[2].x;
+	int diry = _trayectoria[0].y - _trayectoria[2].y;
+
+		if(dirx == 0 || diry == 0) {
+			escalerado = false;
+		}
+		int i = 0;
+
+// vale $ P_c $: $ i = 0 \land \longitud{vueloRealizado(this)} > 2 \land escalerado == (enVuelo(this) \land \mid dirx \mid == 1 \ \land \mid diry \mid == 1) $
+
+		while(escalerado && i < _trayectoria.size() - 2) {
+		
+// invariante I: $ 0 \leq i \leq \longitud{vueloRealizado(this)} -2 \land escalerado == (enVuelo(this) \land (\forall j \leftarrow [0..i -1) $ $ (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx $ $ \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry) $
+
+// variante v: $ \longitud{vueloRealizado(this)} - i - 2 $
+
+// $ cota = 0 $
+
+			escalerado = _trayectoria[i].x - _trayectoria[i + 2].x == dirx
+					 && _trayectoria[i].y - _trayectoria[i + 2].y == diry;
+			++i;
+		}
+
+// vale $ Q_c $: $ escalerado == (enVuelo(this) \land (\forall i \leftarrow [0..\longitud{vueloRealizado(this)} - 2)) $ $ (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx $ $ \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry)) $
+		
+	}
+	return escalerado;
+}
