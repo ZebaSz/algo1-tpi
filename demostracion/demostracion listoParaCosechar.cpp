a4a2c6bcb99dffd97fc5a941e1f2f60d5a11f314
carlos rafael giudice <cgiudice@ws17.labo4>
2016-06-10 16:36:09 -0300

    unos detallesines mas, falta bastannnnnte

--- demostracion/demostracion listoParaCosechar.cpp
+++ demostracion/demostracion listoParaCosechar.cpp
@@ -42,10 +42,10 @@ bool Sistema::listoParaCosechar() const
  
  buscar practica de demostracion de ciclos aca: http://www.dc.uba.ar/materias/aed1/2016/1c/descargas/clases-practicas
  
- 
+
 Pc: cantCosechables == 0 ^ cantCultivos == (_campo.dimensiones().ancho * _campo.dimensiones().largo) -2 ^ i == 0
  
-Qc: i == _campo.dimensiones().ancho * _campo.dimensiones().largo ^ cantCosechables = | [ 1 | j <-- [0..i], estadoParcela(i) == ListoParaCosechar] |
+Qc: i == _campo.dimensiones().ancho * _campo.dimensiones().largo ^ cantCosechables = | [ 1 | j <-- [0..i), estadoParcela(j) == ListoParaCosechar] |
  
 aux estadoParcela(int k) : EstadoCultivo = _estado.parcelas[k/_campo.dimensiones().largo][k % _campo.dimensiones().largo];
  
@@ -53,9 +53,9 @@ B: _campo.dimensiones().ancho * _campo.dimensiones().largo > i
  
 fv: _campo.dimensiones().ancho * _campo.dimensiones().largo - i
  
-cota: 1
+cota: 0
  
-I: 0 <= i <= _campo.dimensiones().ancho * _campo.dimensiones().largo ^ cantCosechables = | [ 1 | j <-- [0..i], estadoParcela(i) == ListoParaCosechar] |
+I: 0 <= i <= _campo.dimensiones().ancho * _campo.dimensiones().largo ^ cantCosechables = | [ 1 | j <-- [0..i), estadoParcela(j) == ListoParaCosechar] |
  
 Pc --> I:
 la precondicion y el invariante estan divididos en tres y dos terminos "andeados" respectivamente:
@@ -69,13 +69,13 @@ los de Pc:
 los de I:
 4) 0 <= i <= _campo.dimensiones().ancho * _campo.dimensiones().largo
  
-5) cantCosechables = | [ 1 | j <-- [0..i], estadoParcela(i) == ListoParaCosechar] |
+5) cantCosechables = | [ 1 | j <-- [0..i), estadoParcela(j) == ListoParaCosechar] |
  
 se justifican 4 y 5 a partir de 1, 2 y 3
  
 3 ==> 4 //porque i == 0 cumple que 0 <= i
  
-1 ^ 3 ==> 5 //porque 3 afirma que i == 0 entonces, en el invariante [0..i] es una lista vacia ==> cantCosechables == 0, que es lo que afirma 1. osea al entrar al ciclo se cumple que cantidad cosechables es 0 por que aun no se han observado parcelas
+1 ^ 3 ==> 5 //porque 3 afirma que i == 0 entonces, en el invariante [0..i) es una lista vacia ==> cantCosechables == 0, que es lo que afirma 1. osea al entrar al ciclo se cumple que cantidad cosechables es 0 por que aun no se han observado parcelas
  
 (I ^ ¬B) ==> Qc:
  
@@ -85,7 +85,7 @@ se justifican 4 y 5 a partir de 1, 2 y 3
 Qc:
 7)i == _campo.dimensiones().ancho * _campo.dimensiones().largo
  
-8) cantCosechables = | [ 1 | j <-- [0..i], estadoParcela(i) == ListoParaCosechar] |
+8) cantCosechables = | [ 1 | j <-- [0..i), estadoParcela(j) == ListoParaCosechar] | //aca uso i para no volver a escribir todo el tamaño del campo, por logica de cortocircuito deberia poder usar 7 como premisa
  
 con 4 5 y 6 se justifican 7 y 8
  
@@ -95,4 +95,5 @@ ambos implican
 i == _campo.dimensiones().ancho * _campo.dimensiones().largo
 y queda probado 7
  
+
  
