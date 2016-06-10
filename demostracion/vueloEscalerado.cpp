3749d20b06de3cfb805ddbc4b15dda4dbcd97b36
ronibarylko <ronibarylko@hotmail.com>
2016-06-10 00:56:28 -0300

    Pc implica I

--- demostracion/vueloEscalerado.cpp
+++ demostracion/vueloEscalerado.cpp
@@ -11,11 +11,11 @@ bool Drone::vueloEscalerado() const
 		int i = 1;
 // vale $ dirx == \prm{vueloRealizado(this)_0} - \prm{vueloRealizado(this)_2} $
 // vale $ diry == \sgd{vueloRealizado(this)_0} - \sgd{vueloRealizado(this)_2} $
-// vale $ P_c $: $ i = 0 \land \longitud{vueloRealizado(this)} > 2 \land escalerado == (enVuelo(this) \land \mid dirx \mid == 1 \ \land \mid diry \mid == 1) $
+// vale $ P_c $: $ i = 1 \land \longitud{vueloRealizado(this)} > 2 \land escalerado == (enVuelo(this) \land |dirx| == |diry|) $
 
 		while(escalerado && i < _trayectoria.size() - 2) {
 		
-// invariante I: $ 0 \leq i \leq \longitud{vueloRealizado(this)} -2 \land escalerado == (enVuelo(this) \land (\forall j \leftarrow [0..i) $ $ (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx $ $ \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry) $
+// invariante I: $ 1 \leq i \leq \longitud{vueloRealizado(this)} -2 \land escalerado == (|dirx| == |diry| \land ((\forall j \leftarrow [0..i) $ $ (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx $ $ \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry) $
 
 // variante v: $ \longitud{vueloRealizado(this)} - i - 2 $
 
