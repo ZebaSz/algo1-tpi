2b72389613a5cc3c68f63c9ebef3867b81412a5e
carlos rafael giudice <cgiudice@ws17.labo4>
2016-06-10 18:30:25 -0300

    the end

--- demostracion/listoParaCosechar.cpp
+++ demostracion/listoParaCosechar.cpp
@@ -1,42 +1,26 @@
 bool Sistema::listoParaCosechar() const
 {
-	int cantCosechables = 0;
-	int cantCultivos = (_campo.dimensiones().ancho * _campo.dimensiones().largo) -2;
-	int x = 0;
+    int cantCosechables = 0;
+    int cantCultivos = (_campo.dimensiones().ancho * _campo.dimensiones().largo) -2;
+    int i = 0;
 
-// vale $ Pc_1 $: $ x == 0$
+// vale $ P_c $: $ cantCosechables == 0 \land i == 0$
 
-	while (_campo.dimensiones().ancho > x) {
-		int y = 0;
+    while (_campo.dimensiones().ancho * _campo.dimensiones().largo > i) {
 
-// invariante $ I_1 $: $ 0 \leq x \leq prm(dimensiones(campo(this))) \land $ $ \longitud{[1| x \leftarrow [0..x), y \leftarrow [0..sgd(dimensiones(campo(this)))), estadoDelCultivo((x,y), this) == ListoParaCosechar]} $ $ == cantCosechables $
+ // invariante $ I$: $ 0 \leq i \leq (prm(dimensiones(campo(this)) \times sgd(dimensiones(campo(this)))) $ $ \land | [1| x \leftarrow [0..i \div sgd(dimensiones(campo(this)))) $ $ y \leftarrow [0.. (i-1) \ mod \ sgd(dimensiones(campo(this)))], estadoDelCultivo((x,y), this) == ListoParaCosechar]| $ $ == cantCosechables $
 
-// variante $ v_1 $: $ prm(dimensiones(campo(this)) - x $
+// variante $ v$: $ prm(dimensiones(campo(this)) \times sgd(dimensiones(campo(this))) - i $
 
-// $ cota1 = 0 $
+// $ cota = 1 $
 
-// vale $ Pc_2 $: $ y = 0 $
+        if(_estado.parcelas[i/_campo.dimensiones().largo][i % _campo.dimensiones().largo] == ListoParaCosechar) {
+            ++cantCosechables;
+        }
+        ++i;
+    }
 
-		while(_campo.dimensiones().largo > y) {
+// vale $ Q_c $: $ i == (prm(dimensiones(campo(this)) \times sgd(dimensiones(campo(this)))) \land $ $ \longitud{[1| pos \leftarrow parcelasCultivo(campo(this)), estadoDelCultivo(pos, this) == ListoParaCosechar]} $ $ == cantCosechables $
 
-// invariante $ I_2 $: $ 0 \leq y \leq sgd(dimensiones(campo(this))) \land $ $ \longitud{[1| x \leftarrow [0..x], y \leftarrow [0..y), estadoDelCultivo(((x,y)), this) == ListoParaCosechar]} $ $ == cantCosechables $
-
-// variante $ v_2 $: $ sgd(dimensiones(campo(this))) - y $
-
-// $ cota2 = 0 $
-
-			if(_estado.parcelas[x][y] == ListoParaCosechar) {
-				++cantCosechables;
-			}
-			++y;
-		}
-
-// vale $ Qc_2 $: $ \longitud{[1| x \leftarrow [0..x], y \leftarrow [0..sgd(dimensiones(campo(this)))), estadoDelCultivo((x,y), this) == ListoParaCosechar]} $ $ == cantCosechables $
-
-		++x;
-	}
-
-// vale $ Qc_1 $: $ \longitud{[1| pos \leftarrow parcelasCultivo(campo(this)), estadoDelCultivo(pos, this) == ListoParaCosechar]} $ $ == cantCosechables $
-
-	return (cantCosechables /(double) cantCultivos >= 0.9);
+    return (cantCosechables /(double) cantCultivos >= 0.9);
 }
