8d6255833272966fc0a61821dee33fbd86e40bfd
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2016-06-10 00:28:09 -0300

    limpie un par de boludeces, creo :v

--- demostracion/vueloEscalerado.cpp
+++ demostracion/vueloEscalerado.cpp
@@ -9,24 +9,26 @@ bool Drone::vueloEscalerado() const
 			escalerado = false;
 		}
 		int i = 1;
-// vale $ dirx == \prm{vueloRealizado(this)_0} - \prm{vueloRealizado(this)_2} $
-// vale $ diry == \sgd{vueloRealizado(this)_0} - \sgd{vueloRealizado(this)_2} $
-// vale $ P_c $: $ i = 0 \land \longitud{vueloRealizado(this)} > 2 \land escalerado == (enVuelo(this) \land \mid dirx \mid == 1 \ \land \mid diry \mid == 1) $
+
+		// estado E1
+		// vale $ dirx == \prm{vueloRealizado(this)_0} - \prm{vueloRealizado(this)_2} $
+		// vale $ diry == \sgd{vueloRealizado(this)_0} - \sgd{vueloRealizado(this)_2} $
+		// vale $ P_c : i == 1 \land enVuelo(this) \land \longitud{vueloRealizado(this)} > 2 \land $
+		//    $ escalerado == (|dirx| == 1 \ \land |diry| == 1) $
 
 		while(escalerado && i < _trayectoria.size() - 2) {
-		
-// invariante I: $ 0 \leq i \leq \longitud{vueloRealizado(this)} -2 \land escalerado == (enVuelo(this) \land (\forall j \leftarrow [0..i) $ $ (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx $ $ \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry) $
 
-// variante v: $ \longitud{vueloRealizado(this)} - i - 2 $
+		// I: $ 1 \leq i \leq \longitud{vueloRealizado(this)} -2 \ \land $
+		//    $ escalerado == (\forall j \leftarrow [0..i)) (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx \ \land $ 
+		//    $ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry) $
+		// v: $ \longitud{vueloRealizado(this)} - i - 2 $
 
-// $ cota = 0 $
-
-			escalerado = _trayectoria[i].x - _trayectoria[i + 2].x == dirx
-					 && _trayectoria[i].y - _trayectoria[i + 2].y == diry;
+			escalerado = _trayectoria[i].x - _trayectoria[i + 2].x == dirx 
+			&& _trayectoria[i].y - _trayectoria[i + 2].y == diry;
 			++i;
 		}
 
-// vale $ Q_c $: $ escalerado == ((\forall i \leftarrow [0..\longitud{vueloRealizado(this)} - 2)) $ $ (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx $ $ \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry)) $
+		// vale $ Q_c : escalerado == (\forall i \leftarrow [0..\longitud{vueloRealizado(this)} - 2)) (\prm{vueloRealizado(this)_i} - \prm{vueloRealizado(this)_{i+2}} == dirx  \land \ \sgd{vueloRealizado(this)_i} - \sgd{vueloRealizado(this)_{i+2}} == diry) $
 
 	}
 	return escalerado;
